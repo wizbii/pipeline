36634d1e54791c3a319d1c16973f4ef399820a36
Rémi Alvado <remi.alvado@wizbii.com>
2016-02-02 16:52:55 +0100

    chore(symfony): do not depend on Symfony for tests

--- Tests/BaseTestCase.php
+++ Tests/BaseTestCase.php
@@ -2,25 +2,7 @@
 
 namespace Wizbii\PipelineBundle\Tests;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
-class BaseTestCase extends WebTestCase
+class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
 
-    protected function setup()
-    {
-        self::$kernel = self::createKernel();
-        self::$kernel->boot();
-    }
-
-    protected function getService($serviceId)
-    {
-        return static::$kernel->getContainer()->get($serviceId, ContainerInterface::NULL_ON_INVALID_REFERENCE);
-    }
-
-    protected function getParameter($parameterId)
-    {
-        return static::$kernel->getContainer()->getParameter($parameterId);
-    }
 }
